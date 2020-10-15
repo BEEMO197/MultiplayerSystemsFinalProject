d46da83e29765e61fd9601d50e1b87a9118bb91c
ShadowOracle19 <lucas.g@hotmail.ca>
2020-10-15 17:42:02 -0400

    added health bar

---
+++ Assets/Scrips/HealthBar.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class HealthBar : MonoBehaviour
+{
+    public Slider slider;
+
+    public int health;
+    public int maxHealth;
+
+    public void SetMaxHealth()
+    {
+        slider.maxValue = maxHealth;
+        slider.value = health;
+    }
+
+    public void SetHealth()
+    {
+        slider.value = health;
+    }
+
+    
+
+}
