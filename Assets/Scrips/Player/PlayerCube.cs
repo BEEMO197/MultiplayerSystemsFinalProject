79c5a83ab248db8e0b16968a18249393b34e634f
Matthew Pereira <BEEMO197@hotmail.com>
2020-10-16 23:38:08 -0400

    Fix
    Fixed Cube Prefab, and Network Manager

--- Assets/Scrips/Player/PlayerCube.cs
+++ Assets/Scrips/Player/PlayerCube.cs
@@ -9,14 +9,11 @@ public class PlayerCube : Character
     public NetworkMan netWorkManRef;
     public NetworkMan.Player playerRef;
     public Camera cubeCamera;
-<<<<<<< Updated upstream:Assets/Scrips/PlayerCube.cs
-    public Vector3 Velocity;
+
     public GameObject bulletRef;
-=======
     public Rigidbody rigidBody;
     public float speed = 5.0f;
     public Character characterRef;
->>>>>>> Stashed changes:Assets/Scrips/Player/PlayerCube.cs
 
     // Start is called before the first frame update
     void Start()
@@ -32,16 +29,10 @@ public class PlayerCube : Character
     {
         if (netWorkManRef.uniqueID.uniqueID == playerRef.id)
         {
-<<<<<<< Updated upstream:Assets/Scrips/PlayerCube.cs
-            Velocity.x = Input.GetAxis("Horizontal");
-            Velocity.z = Input.GetAxis("Vertical");
-
-=======
             //Velocity.x = Input.GetAxis("Horizontal");
             //Velocity.z = Input.GetAxis("Vertical");
             rigidBody.velocity = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
             rigidBody.velocity *= speed;
->>>>>>> Stashed changes:Assets/Scrips/Player/PlayerCube.cs
                 
             //transform.position += Velocity;
 
