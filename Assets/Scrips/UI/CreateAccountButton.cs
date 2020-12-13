59321856c3a15089ae39804967dd9bc33a3d481f
ShadowOracle19 <lucas.g@hotmail.ca>
2020-12-13 01:12:48 -0500

    stuff

--- Assets/Scrips/UI/CreateAccountButton.cs
+++ Assets/Scrips/UI/CreateAccountButton.cs
@@ -4,17 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 public class CreateAccountButton : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
     public void OnButtonPress()
     {
         SceneManager.LoadScene("Create Account");
