f062debcd35535eaaa5e3c5a873db1cd2f719cef
Gabtwotwo <Gabtwotwo@github.com>
2020-12-13 19:07:36 -0500

    Enemy spawner

---
+++ Assets/Scrips/Enemy/EnemyManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class EnemyManager : MonoBehaviour
+{
+
+    public GameObject enemy;
+    public int maxEnemies;
+
+    private Queue<GameObject> enemyPool;
+    // Start is called before the first frame update
+    void Start()
+    {
+        BuildEnemyPool();
+    }
+
+    private void BuildEnemyPool()
+    {
+        enemyPool = new Queue<GameObject>();
+
+        for(int i = 0; i < maxEnemies; i++)
+        {
+            var tempEnemy = Instantiate(enemy);
+            tempEnemy.SetActive(false);
+            tempEnemy.transform.parent = transform;
+            enemyPool.Enqueue(tempEnemy);
+        }
+    }
+
+    public GameObject GetEnemy(Vector3 position)
+    {
+
+        var newEnemy = enemyPool.Dequeue();
+        newEnemy.SetActive(true);
+        newEnemy.transform.position = position;
+
+        return newEnemy;
+        
+
+
+         
+        
+    }
+
+    public void ReturnEnemy(GameObject returnedEnemy)
+    {
+        returnedEnemy.SetActive(false);
+        enemyPool.Enqueue(returnedEnemy);
+    }
+}
--- Assets/Scrips/Enemy/EnemySpawner.cs
+++ Assets/Scrips/Enemy/EnemySpawner.cs
@@ -7,15 +7,29 @@ public class EnemySpawner : MonoBehaviour
     NetworkClient connectedClient;
     NetworkObjects.NetworkPlayer connectedPlayer;
 
+    public EnemyManager enemyManager;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
+
+    
+
 
     // Update is called once per frame
     void Update()
     {
-        
+        SpawnEnemy();
+    }
+
+
+    private void SpawnEnemy()
+    {
+        if(Time.frameCount % Random.Range(200, 700) == 0)
+        {
+            enemyManager.GetEnemy(new Vector3(Random.Range(-100.0f, 100.0f), Random.Range(-100.0f, 100.0f), 1.0f));
+        }
     }
 }
