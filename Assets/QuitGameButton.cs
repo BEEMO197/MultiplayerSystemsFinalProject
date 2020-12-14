3cfe92549aecc0d550f2c95638b2a1c2f284c926
ShadowOracle19 <lucas.g@hotmail.ca>
2020-12-14 00:32:14 -0500

    added quit button

---
+++ Assets/QuitGameButton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuitGameButton : MonoBehaviour
+{
+    public void OnButtonPress()
+    {
+        Application.Quit();
+        //UnityEditor.EditorApplication.isPlaying = false;
+    }
+}
